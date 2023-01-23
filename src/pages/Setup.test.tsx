d8429b81c66e33d081a998f3affc82d4bc2d16f8
BVC SME 01 <118875477+bvc-sme01@users.noreply.github.com>
2023-01-22 17:43:27 -0700

    Add more tests

--- src/pages/Setup.test.tsx
+++ src/pages/Setup.test.tsx
@@ -1,4 +1,5 @@
 import * as github from '../services/github';
+import * as usersAPI from '../services/users';
 import { AxiosError } from 'axios';
 
 import { RenderResult, fireEvent, render, screen, waitFor } from '@testing-library/react-native';
@@ -12,15 +13,16 @@ describe(Setup.name, () => {
     describe('username validation', () => {
         let input: ReturnType<RenderResult['getByTestId']>;
         let button: ReturnType<RenderResult['getByTestId']>;
+        let alertSpy: jest.SpyInstance;
 
         beforeEach(() => {
             render(<Setup {...mockProps} />);
             input = screen.getByTestId('input');
             button = screen.getByTestId('button');
+            alertSpy = jest.spyOn(Alert, 'alert');
         });
 
         it('shows error if username is not on GitHub', async () => {
-            const alertSpy = jest.spyOn(Alert, 'alert');
             jest.spyOn(github, 'getUserInfo').mockRejectedValueOnce({
                 isAxiosError: true,
                 response: { status: 404 },
@@ -31,6 +33,34 @@ describe(Setup.name, () => {
 
             await waitFor(() => {
                 expect(alertSpy).toHaveBeenCalledWith('There is no such username on GitHub.');
+            });
+        });
+
+        describe('when username is on GitHub', () => {
+            beforeEach(() => {
+                jest.spyOn(github, 'getUserInfo').mockResolvedValue({});
+            });
+
+            it('POSTs user to API', async () => {
+                let postUserSpy = jest.spyOn(usersAPI, 'postUser').mockResolvedValue({} as any);
+
+                fireEvent.changeText(input, 'foo');
+                fireEvent.press(button);
+
+                await waitFor(() => {
+                    expect(postUserSpy).toHaveBeenCalled();
+                });
+            });
+
+            it('shows error when users API is unavailable', async () => {
+                let postUserSpy = jest.spyOn(usersAPI, 'postUser').mockRejectedValue("Example Error");
+
+                fireEvent.changeText(input, 'foo');
+                fireEvent.press(button);
+
+                await waitFor(() => {
+                    expect(alertSpy).toHaveBeenCalledWith("Example Error");
+                });
             });
         });
     });
